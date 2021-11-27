b73ef0111a6801f320c2478909f1d1107967b93e
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-11-27 09:38:38 +0530

    initial commit

---
+++ src/main/java/com/demo/classmanagerapi/ClassManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.classmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/classmanagerapi/ClassManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.classmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
